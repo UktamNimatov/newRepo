cbbb70eb8dc44a7a33ee7ecd9a6058c257ad06a6
Uktam Nimatov <uktam.nimatov@gmail.com>
2019-07-17 17:06:33 +0500

    hello1 is somehow changed

--- src/main/java/payme/making_payments/Hello1.java
+++ src/main/java/payme/making_payments/Hello1.java
@@ -3,6 +3,7 @@ package payme.making_payments;
 public class Hello1 {
     public static void main(String[] args) {
         System.out.println("Hello World 1");
-        System.out.println("I am a hacker");
+        System.out.println("So, you are not a hacker");
+        System.out.println("I am a hacker, only");
     }
 }
